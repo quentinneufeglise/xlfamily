f933fed3638cfd741602c7af620ccb19360eefcd
Aneliya <aneliya@xlfamily.com>
2016-08-29 17:14:13 +0200

    Fixed the missing slider

--- static/assets/js/menu.js
+++ static/assets/js/menu.js
@@ -5,23 +5,26 @@
 
 $(document).ready(function() {
 
+    $("body").click(function () {
 
-// Close menu on  click outside of body
 
-$("body").on("click",function (event) {
-    var target = event.target;
-
-    if ( $(target).is('.menu-wrapper') || $(target).is('.hamburger')){
-        // menu
-    } else {
-        if($(target).parents('.menu-wrapper').length > 0){
-            // menu
-        } else {
-            // not menu
-            $(".menu-wrapper").removeClass('menu-open');
+        if( this.class != 'menu-wrapper' && $(".hamburger").hasClass("hamburger-open")) {
+            $(".menu-wrapper").hide();
+            $(".hamburger").removeClass("hamburger-open")
+            console.log("11")
         }
-    }
-});
+    });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -40,3 +43,4 @@ $("body").on("click",function (event) {
         $(".menu-wrapper").removeClass('menu-open');
     });
 });
+
