7d04cea01a7ed82dcfc328832a84c96f32e98c19
Aneliya <aneliya@xlfamily.com>
2016-09-06 10:53:47 +0200

    Edited a bug in the experiments on the homepage

--- static/assets/mail/contact_me.php
+++ static/assets/mail/contact_me.php
@@ -2,28 +2,20 @@
 
 
 
-
-
-$ideaname = $_POST['ideaname'];
-$how = $_POST['how'];
-$hotspot = $_POST['hotspot'];
-$category = $_POST['category'];
-$idea = $_POST['idea'];
 $name = $_POST['name'];
-$role = $_POST['role'];
-$center = $_POST['center'];
-$owners = $_POST['owners'];
-$country = $_POST['country'];
 $email_address = $_POST['email'];
+$phone_number = $_POST['number'];
+$thoughts = $_POST['thoughts'];
+
 
 
 
 	
 // Create the email and send the message
-$to = 'ragna@xlfamily.com';
-$email_subject = "Power up the future  :  $name";
-$email_body = "Er is een nieuw idee binnen!! \n\n"."Hier zijn de details:\n\n Idee naam: $ideaname \n\n Hoe voegt het idee waarde toe : $how \n\n Focus: $hotspot \n\n.Categorie : $category \n\n Idee omschrijving: $idea\n\n Naam deelnemer: $name \n\n Rol: $role \n\n Afdeling : $center \n\n Email adres: $email_address";
-$headers = "From: noreply@powerupthefuture.nl\n";
+$to = 'aneliya@xlfamily.com';
+$email_subject = "Contact request XL website  :  $name";
+$email_body = "Someone is interested in XL family :) \n\n"."Details\n\n Name: $name \n\n Email address : $email_address \n\n Phone number: $hotspot \n\n.Message : $thoughts \n\n Idee omschrijving: $idea\n\n Naam deelnemer: $name \n\n Rol: $role \n\n Afdeling : $center \n\n Email adres: $email_address";
+$headers = "From: noreply@xlfamily.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
