79be5c6a440e419ec6f63160ed42d37c1f631565
Aneliya <aneliya@xlfamily.com>
2016-09-07 12:42:24 +0200

    fixing bugs - menu links and team carousel bugs

--- static/assets/mail/newsletter.php
+++ static/assets/mail/newsletter.php
@@ -10,7 +10,7 @@ $nl_email_address = $_POST['email'];
 
 
 // Create the email and send the message
-$to = 'aneliya@xlfamily.com';
+$to = 'silvio@xlfamily.com';
 $email_subject = "New subacriber to the newsletter  :  $nl_name";
 $email_body = "Someone wants to be in our newsletter \n\n"."Details\n\n Name: $nl_name \n\n Email address : $nl_email_address";
 $headers = "From: noreply@xlfamily.com\n";
