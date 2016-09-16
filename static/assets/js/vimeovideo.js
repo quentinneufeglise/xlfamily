a23df0104790370a161ab453b82a35564e3a36e9
Aneliya <aneliya@xlfamily.com>
2016-09-16 20:06:33 +0200

    intermediate progress on the website content edits. NOTE!!! JS error - menu not working

---
+++ static/assets/js/vimeovideo.js
@@ -0,0 +1,44 @@
+/*===VIMEO VIDEO===*/
+
+$(document).ready(function() {
+
+
+    var iframe = document.querySelector('iframe');
+    var player = new Vimeo.Player(iframe);
+
+
+
+    $("#playbutton").click(function(){
+        $(".home-video").fadeIn(800);
+        $(".menu-cross4").fadeIn(800);
+        $("body").addClass("no-scroll");
+
+    });
+
+
+    $(".menu-cross4").click(function(){
+        $(".home-video").fadeOut(800);
+        $(".menu-cross4").fadeOut(800);
+        $("body").removeClass("no-scroll");
+        player.unload();
+
+    });
+
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+
+
+
+
+});
+
+
+
